9b5b44c460b918ecae50ef788fb2f4a0b00e4654
L3onard004 <leonardo.lorenzin.1@studenti.unipd.it>
2025-05-06 13:14:39 +0200

    Correzione IntegerToRoman

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -9,7 +9,6 @@ public class IntegerToRoman {
         if (number < 1 || number > 1000) {
             throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
         }
-
         // Definizione delle lettere romane e dei loro valori corrispondenti
         String[] romanLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -17,20 +17,20 @@ public class IntegerToRomanTest {
         assertEquals("I", IntegerToRoman.convert(1));
         assertEquals("II", IntegerToRoman.convert(2));
         assertEquals("III", IntegerToRoman.convert(3));
-	assertEquals("IV", IntegerToRoman.convert(4));
+	    assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
         assertEquals("VI", IntegerToRoman.convert(6));
         assertEquals("VII", IntegerToRoman.convert(7));
         assertEquals("VIII", IntegerToRoman.convert(8));
         assertEquals("IX", IntegerToRoman.convert(9));
         assertEquals("X", IntegerToRoman.convert(10));
-	assertEquals("XV", IntegerToRoman.convert(15));
+	    assertEquals("XV", IntegerToRoman.convert(15));
         assertEquals("XIX", IntegerToRoman.convert(19));
         assertEquals("XX", IntegerToRoman.convert(20));
         assertEquals("XXX", IntegerToRoman.convert(30));
         assertEquals("XL", IntegerToRoman.convert(40));
         assertEquals("L", IntegerToRoman.convert(50));
-	assertEquals("LX", IntegerToRoman.convert(60));
+	    assertEquals("LX", IntegerToRoman.convert(60));
         assertEquals("LXX", IntegerToRoman.convert(70));
         assertEquals("LXXX", IntegerToRoman.convert(80));
         assertEquals("XC", IntegerToRoman.convert(90));
@@ -39,7 +39,7 @@ public class IntegerToRomanTest {
         assertEquals("CCC", IntegerToRoman.convert(300));
         assertEquals("CD", IntegerToRoman.convert(400));
         assertEquals("D", IntegerToRoman.convert(500));
-	assertEquals("DC", IntegerToRoman.convert(600));
+	    assertEquals("DC", IntegerToRoman.convert(600));
         assertEquals("DCC", IntegerToRoman.convert(700));
         assertEquals("DCCC", IntegerToRoman.convert(800));
         assertEquals("CM", IntegerToRoman.convert(900));
@@ -95,23 +95,5 @@ public class IntegerToRomanTest {
             assertEquals("Errore nella conversione bidirezionale di " + i, i, backToInteger);
         }
     }
-   @Test(expected = IllegalArgumentException.class)
-    public void testMalformedRomanInput_IIII() {
-        romanToInteger("IIII");  // numero scorretto
-    }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testMalformedRomanInput_InvalidChar() {
-        romanToInteger("ABCD");  // caratteri non validi
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testMalformedRomanInput_EmptyString() {
-        romanToInteger("");  // stringa vuota
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testMalformedRomanInput_Null() {
-        romanToInteger(null);  // input null
-    }
 }
